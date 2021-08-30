4f3be984a60d86fbbed7fccd3e27fbaca0b12ed8
huidadao <huidadao@gmail.com>
2021-08-30 18:05:21 +0900

    updated

--- crash_course_Traversy/dist/index.js
+++ crash_course_Traversy/dist/index.js
@@ -1,22 +1,21 @@
-"use strict";
 // Basic Types
-let id = 5;
-let company = 'Hui Media';
-let isPublished = true;
-let x = 'Hello';
-let ids = [1, 2, 3, 4, 5];
-let arr = [1, true, 'Hello'];
+var id = 5;
+var company = 'Hui Media';
+var isPublished = true;
+var x = 'Hello';
+var ids = [1, 2, 3, 4, 5];
+var arr = [1, true, 'Hello'];
 // Tuple
-let person = [1, 'Alex', true];
+var person = [1, 'Alex', true];
 // Tuple Array
-let employee;
+var employee;
 employee = [
     [1, 'Alex'],
     [2, 'John'],
     [3, 'Brad'],
 ];
 // Union
-let pid;
+var pid;
 pid = '22';
 // Enum
 var Direction1;
@@ -26,4 +25,41 @@ var Direction1;
     Direction1[Direction1["Left"] = 3] = "Left";
     Direction1[Direction1["Right"] = 4] = "Right";
 })(Direction1 || (Direction1 = {}));
-console.log(Direction1.Left);
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+var user = {
+    id: 1,
+    name: 'John'
+};
+// Type Assertion
+var cid = 1;
+// let customerId = <number>cid
+var customerId = cid;
+// Functions
+function addNum(x, y) {
+    return x + y;
+}
+// Void
+function log(message) {
+    console.log(message);
+}
+var user1 = {
+    id: 1,
+    name: 'John'
+};
+var add = function (x, y) { return x + y; };
+var sub = function (x, y) { return x - y; };
+// Classes
+var Person = /** @class */ (function () {
+    function Person() {
+        console.log(123);
+    }
+    return Person;
+}());
+var brad = new Person();
+var mike = new Person();
--- crash_course_Traversy/src/index.ts
+++ crash_course_Traversy/src/index.ts
@@ -31,4 +31,66 @@ enum Direction1 {
   Right
 }
 
-console.log(Direction1.Left)
+enum Direction2 {
+  Up = 'Up',
+  Down = 'Down',
+  Left = 'Left',
+  Right = 'Right'
+}
+
+// Objects
+type User = {
+  id: number
+  name: string
+}
+
+const user: User = {
+  id: 1,
+  name: 'John'
+}
+
+// Type Assertion
+let cid: any = 1
+// let customerId = <number>cid
+let customerId = cid as number
+
+// Functions
+function addNum(x:number, y:number): number {
+  return x + y
+}
+
+// Void
+function log(message: string | number): void {
+  console.log(message)
+}
+
+// Interfaces
+interface UserInterface {
+  readonly id: number
+  name: string
+  age?: number
+}
+
+const user1: UserInterface = {
+  id: 1,
+  name: 'John'
+}
+
+interface MathFunc {
+  (x: number, y: number): number
+}
+const add: MathFunc = (x: number, y: number): number => x + y
+const sub: MathFunc = (x: number, y: number): number => x - y
+
+// Classes
+class Person {
+  id: number
+  name: string
+
+  constructor() {
+    console.log(123)
+  }
+}
+
+const brad = new Person()
+
